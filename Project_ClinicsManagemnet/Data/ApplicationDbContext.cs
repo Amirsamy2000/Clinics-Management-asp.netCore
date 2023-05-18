c7890ba27177c2d92d3bfc34135b4dfedb1f3d8d
ALameer Azmy <alameersamy99@outlook.com>
2023-05-18 19:25:50 +0300

    Add project files.

---
+++ Project_ClinicsManagemnet/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_ClinicsManagemnet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Project_ClinicsManagemnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_ClinicsManagemnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
